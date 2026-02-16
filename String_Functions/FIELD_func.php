cac0675ca082f01915ce3be128004893c2225b23
pagalanbryljames-star <pagalanbryljames@gmail.com>
2026-02-16 17:43:08 +0800

    Add files via upload

---
+++ String_Functions/FIELD_func.php
@@ -0,0 +1,54 @@
+<?php
+include '../db.php';
+$sql = "SELECT fname, lname, email,FIELD('David',fname) as indexed FROM customers;";
+$result = $conn->query($sql);
+
+?>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    
+<div class='container mt-5'>
+<div class= d-flex justify-content-between align-items-center mb-4>
+<h2>Output for FIELD() Function</h2>
+<div class=ms-auto><a class='btn btn-outline-success' href='../index.php'>Back to Home</a></div>
+</div>
+
+<div class="p-4 rounded-3 mb-4 shadow-sm" style="background-color: #f8f9fa;">
+    <p class="mb-0"><strong>SQL QUERY:</strong>
+    <br><code><?php echo $sql?></code></p>
+</div>
+<table class='table table-striped table-hover rounded-3 overflow-hidden shadow-sm'>
+    <thead style='background-color:#04AA6D; color:white'>
+        <tr>
+            <th class=text-center>First Name</th>
+            <th class=text-center>Last Name</th>
+            <th class=text-center>Email</th>
+        </tr>
+    </thead>
+    
+<?php
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $highlight = ($row["indexed"]>0)? "style ='background-color:yellow;'":"";
+
+        echo "<tr $highlight>";
+        echo "<td class='text-center'>" . htmlspecialchars($row["fname"]) . "</td>";
+        echo "<td class='text-center'>" . htmlspecialchars($row["lname"]) . "</td>";
+        echo "<td class='text-center'>" . htmlspecialchars($row["email"]) . "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='2'>0 results found or query failed.</td></tr>";
+}
+echo "</table>";
+echo "</div>";
+
+
+$conn->close();
+
+?>
+</body>
